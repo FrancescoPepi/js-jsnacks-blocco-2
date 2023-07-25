a4d8fce9188536fd3a8315b0dc9f7daad631ea58
FrancescoPepi <francescopepi16@gmail.com>
2023-07-25 18:02:11 +0200

    es: bONUS!

--- JS/js-nack-4.js
+++ JS/js-nack-4.js
@@ -1,32 +1,71 @@
+//  # VERSIONE SEMPLICE!
+// // Dichiaro un Array InsiemeUno (5 elemnti)
+// let InsiemeUno = [2, 4, 7, 3, 8];
+
+// // Dichiaro un Array InsiemeDue (7 elementi)
+// let InsiemeDue = [5, 7, 2, 98, 1, 53, 24];
+
+// let InsiemeGrande;
+
+// if (InsiemeUno.length > InsiemeDue.length) {
+//   InsiemeGrande = InsiemeUno;
+// } else {
+//   InsiemeGrande = InsiemeDue;
+// }
+
+// console.log(InsiemeGrande);
+
+// for (let i = 0; i < InsiemeGrande.length; i++) {
+//   const NumberRandom = Math.floor(Math.random() * 100) + 1;
+//   console.log(NumberRandom + "random nummero");
+
+//   if (InsiemeUno.length == InsiemeDue.length) {
+//     break;
+//   } else if (InsiemeUno.length > InsiemeDue.length) {
+//     InsiemeDue.push(NumberRandom);
+//     console.log(NumberRandom + "random nummero aggiunto 1");
+//   } else if (InsiemeUno.length < InsiemeDue.length) {
+//     InsiemeUno.push(NumberRandom);
+//     console.log(NumberRandom + "random nummero aggiunto 2");
+//   }
+// }
+// console.log(InsiemeDue);
+// console.log(InsiemeUno);
+
+// # VERSIONE UN PO PIù COMPLESSA
+
 // Dichiaro un Array InsiemeUno (5 elemnti)
-let InsiemeUno = [2, 4, 7, 3, 8];
+let InsiemeUno = [];
 
 // Dichiaro un Array InsiemeDue (7 elementi)
 let InsiemeDue = [5, 7, 2, 98, 1, 53, 24];
 
-let InsiemeGrande;
-
-if (InsiemeUno.length > InsiemeDue.length) {
-  InsiemeGrande = InsiemeUno;
+const NumberInput = parseInt(prompt("Inserisci un numero da 8 a 100"));
+if (isNaN(NumberInput) || NumberInput < 8 || NumberInput > 100) {
+  const NumberInput = parseInt(prompt("Inserisci un numero da 8 a 100"));
 } else {
-  InsiemeGrande = InsiemeDue;
-}
+  console.log(NumberInput);
 
-console.log(InsiemeGrande);
-
-for (let i = 0; i < InsiemeGrande.length; i++) {
-  const NumberRandom = Math.floor(Math.random() * 100) + 1;
-  console.log(NumberRandom + "random nummero");
-
-  if (InsiemeUno.length == InsiemeDue.length) {
-    break;
-  } else if (InsiemeUno.length > InsiemeDue.length) {
-    InsiemeDue.push(NumberRandom);
-    console.log(NumberRandom + "random nummero aggiunto 1");
-  } else if (InsiemeUno.length < InsiemeDue.length) {
+  for (let i = 0; i < NumberInput; i++) {
+    const NumberRandom = Math.floor(Math.random() * 100) + 1;
+    // console.log(NumberRandom + "random nummero 1");
     InsiemeUno.push(NumberRandom);
-    console.log(NumberRandom + "random nummero aggiunto 2");
   }
+
+  if (InsiemeUno.length == NumberInput) {
+    for (let i = 0; i < InsiemeUno.length; i++) {
+      const NumberRandom = Math.floor(Math.random() * 100) + 1;
+      //   console.log(NumberRandom + "random nummero");
+
+      if (InsiemeUno.length == InsiemeDue.length) {
+        break;
+      } else if (InsiemeUno.length > InsiemeDue.length) {
+        InsiemeDue.push(NumberRandom);
+        // console.log(NumberRandom + "random nummero aggiunto 2");
+      }
+    }
+  }
+
+  console.log(InsiemeUno);
+  console.log(InsiemeDue);
 }
-console.log(InsiemeDue);
-console.log(InsiemeUno);
